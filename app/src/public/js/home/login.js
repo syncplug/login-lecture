968e6b3374f9419b2a44ccd88e70be138e05922c
syncplug <nemonic@naver.com>
2021-08-15 13:54:40 +0900

    DOM 적용

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,15 @@
 "use strict"
 
-console.log("login.js");
+const id = document.querySelector("#id"),
+    	pw = document.querySelector("#pw"),
+			button = document.querySelector("button");
+
+button.addEventListener("click", login);
+function login(){
+	const req = {
+		id: id.value,
+		pw: pw.value
+	}
+console.log(req);
+}
+
